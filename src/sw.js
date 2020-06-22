0b7bb226a182369dd6bf05bd7d82449f91e7f96c
hitenderravi <hitender.h.ravi@gmail.com>
2020-06-22 17:41:25 +0530

    update stuff

--- src/sw.js
+++
@@ -1,8 +0,0 @@
-// console.log('service worker wake up')
-// self.addEventListener('install',event=>{
-//     console.log('service installed')
-// })
-// self.addEventListener('active',event=>{
-//     console.log('service activated')
-// })
-self.importScripts('node_modules/sw-toolbox/sw-toolbox.js')
